66899a7a043c35c354d8a258493c13be972761bb
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-03 19:22:33 +0530

    arch: aarch64: add ExecutionContext and context_switch procedure
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

---
+++ kernel/include/arch/aarch64/context.h
@@ -0,0 +1,17 @@
+#ifndef _CONTEXT_H_
+#define _CONTEXT_H_
+
+#include "types.h"
+#include "common_defs.h"
+
+typedef struct {
+	u64 x19, x20, x21, x22, x23, x24, x25, x26, x27, x28,
+		fp, // x29
+		lr, // x30
+		sp;
+} ExecutionContext;
+
+static_assert(sizeof(ExecutionContext) == 104, "ExecutionContext is not 104 "
+					       "bytes?");
+
+#endif // _CONTEXT_H_
