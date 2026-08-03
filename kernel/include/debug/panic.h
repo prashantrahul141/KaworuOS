a5ba1bb4871acc0208affaa1236cb418d9a0d75c
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-03 19:55:39 +0530

    debug: panic: disable all interrupts when panicking
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- kernel/include/debug/panic.h
+++ kernel/include/debug/panic.h
@@ -9,6 +9,7 @@
 	do {                               \
 		FATAL(fmt, ##__VA_ARGS__); \
 		brk(0);                    \
+		w_intrall_disable();       \
 		wfi();                     \
 		UNREACHABLE();             \
 	} while (0)
