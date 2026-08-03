94b7948064b6bcc028507eae5754c91d215c3547
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-03 12:19:24 +0530

    debug: log: refactor out buffer size for each log event
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- kernel/include/debug/log.h
+++ kernel/include/debug/log.h
@@ -24,20 +24,23 @@
 	#define LEVEL LEVEL_ERROR
 #endif
 
+constexpr usize individual_buffer_sizes = (1 << 10);
+
 static inline void __log(const i8 *level_str, const IOColor color,
 			 const i8 *file, const i8 *func, usize line,
 			 const i8 *fmt, ...)
 {
-	i8 buffers[3][(1 << 10)];
+	i8 buffers[3][individual_buffer_sizes];
 	IOEvent events[3];
-	usize wrote = vsnprintf((i8 *)buffers[0], 2048, "[ %s ]", level_str);
+	usize wrote = vsnprintf((i8 *)buffers[0], individual_buffer_sizes,
+				"[ %s ]", level_str);
 	events[0].bg = color;
 	events[0].fg = IO_DEFAULT_COLOR_BG;
 	events[0].len = wrote;
 	events[0].msg = (i8 *)&buffers[0];
 
-	wrote = vsnprintf((i8 *)buffers[1], 2048, " %s:%s:%d: ", file, func,
-			  line);
+	wrote = vsnprintf((i8 *)buffers[1], individual_buffer_sizes,
+			  " %s:%s:%d: ", file, func, line);
 	events[1].bg = IO_DEFAULT_COLOR_BG;
 	events[1].fg = color;
 	events[1].len = wrote;
@@ -45,7 +48,8 @@ static inline void __log(const i8 *level_str, const IOColor color,
 
 	va_list args;
 	va_start(args, fmt);
-	wrote = __vsnprintf((i8 *)buffers[2], 2048, fmt, args);
+	wrote = __vsnprintf((i8 *)buffers[2], individual_buffer_sizes, fmt,
+			    args);
 	events[2].bg = IO_DEFAULT_COLOR_BG;
 	events[2].fg = color;
 	events[2].len = wrote;
