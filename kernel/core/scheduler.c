57575b4eb01dcfaae9a336905eeda219959afdfe
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-05 03:11:13 +0530

    core: scheduler: add an extremely simple round-robin style tasks scheduler
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

---
+++ kernel/core/scheduler.c
@@ -0,0 +1,64 @@
+#include "core/scheduler.h"
+#include "aarch64/context.h"
+#include "core/cpu.h"
+#include "core/task.h"
+#include "debug/log.h"
+#include "debug/panic.h"
+
+void yield(void)
+{
+	schedule();
+}
+
+void schedule(void)
+{
+	Cpu *cpu = this_cpu();
+	DEBUG("schedule cpu = %d", cpu->cpuid);
+
+	Task *prev = cpu->current;
+
+	/*
+	 * if the current task is still runnable, put it back on the
+	 * run queue before selecting the next task
+	 */
+	if (nullptr != prev && prev != cpu->idle &&
+	    TASK_RUNNING == prev->state) {
+		prev->state = TASK_READY;
+		run_queue_enqueue(&cpu->tasks, prev);
+	}
+
+	/* pick the next task */
+	Task *next = run_queue_dequeue(&cpu->tasks);
+
+	if (next == nullptr) {
+		next = cpu->idle;
+	}
+
+	next->state = TASK_RUNNING;
+	cpu->current = next;
+
+	/*
+	 * if we are already running the chosen task, there is nothing to do
+	 * this happens when the run queue is empty and we're already in idle
+	 */
+	if (prev == next) {
+		return;
+	}
+
+	if (nullptr != prev) {
+		context_switch(&prev->context, &next->context);
+
+		/*
+		 * we resume execution here when this task is scheduled again.
+		 */
+		return;
+	}
+
+	/*
+	 *  first task ever scheduled on this cpu
+	 */
+	ExecutionContext bootstrap;
+	context_switch(&bootstrap, &next->context);
+
+	panic("bootstrap context returned");
+}
---
+++ kernel/include/core/scheduler.h
@@ -0,0 +1,8 @@
+#ifndef _SCHEDULER_H_
+#define _SCHEDULER_H_
+
+void schedule(void);
+
+void yield(void);
+
+#endif // _SCHEDULER_H_
