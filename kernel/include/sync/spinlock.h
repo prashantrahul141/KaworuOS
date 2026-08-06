eb217920686d833c270ee43ff85e8217af3f516f
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-06 13:33:36 +0530

    sync: spinlock: add spinlock_acquire_scoped
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- kernel/include/sync/spinlock.h
+++ kernel/include/sync/spinlock.h
@@ -3,6 +3,7 @@
 
 /* spinlock implementation */
 
+#include "common_defs.h"
 #include "types.h"
 #include "config.h"
 
@@ -20,5 +21,23 @@ typedef struct {
 void spinlock_init(SpinLock *sp, const i8 *name);
 void spinlock_acquire(SpinLock *sp);
 void spinlock_release(SpinLock *sp);
+
+USED static void _spinlock_autocleanup(SpinLock **sp)
+{
+	if (nullptr != sp && nullptr != *sp) {
+		spinlock_release(*sp);
+	}
+}
+
+#define _SPINLOCK_SCOPED_IMPL(sp, n)           \
+	spinlock_acquire((sp));                \
+	SpinLock *CONCAT(_spinlock_scoped_, n) \
+		__attribute__((cleanup(_spinlock_autocleanup))) = (sp)
+
+/*
+ * locks the spinlock and automatically unlocks it at the end of scope.
+ * sp : pointer to the spinlock
+ */
+#define spinlock_acquire_scoped(sp) _SPINLOCK_SCOPED_IMPL(sp, __LINE__)
 
 #endif // _SPINLOCK_H_
