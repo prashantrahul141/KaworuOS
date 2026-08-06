dd961a9515e5fbaf185f88c7fd6c28f4234bf5a8
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-06 17:43:43 +0530

    lib: common_defs: add container_of
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- lib/include/common_defs.h
+++ lib/include/common_defs.h
@@ -1,6 +1,8 @@
 #ifndef _COMMON_DEFS_H_
 #define _COMMON_DEFS_H_
 
+#include "types.h"
+
 /*
  * shamelessly stolen from:
  * https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/include/linux/math.h?id=ddd664bbff63e09e7a7f9acae9c43605d4cf185f#n10
@@ -18,6 +20,8 @@
 #define PACKED	       __attribute__((packed))
 #define USED	       __attribute__((used))
 #define UNUSED	       __attribute__((unused))
+
+#define NO_SANITIZE(rule) __attribute__((no_sanitize(rule)))
 
 #define IS_ALIGNED(value, alignment) ((value) % (alignment) == 0)
 
@@ -42,5 +46,14 @@
 
 #define _CONCAT2(a, b) a##b
 #define CONCAT(a, b)   _CONCAT2(a, b)
+
+NO_SANITIZE("alignment")
+USED static inline void *container_of_impl(void *ptr, usize off)
+{
+	return (char *)ptr - off;
+}
+
+#define container_of(ptr, type, member) \
+	((type *)container_of_impl((ptr), offsetof(type, member)))
 
 #endif // _COMMON_DEFS_H_
