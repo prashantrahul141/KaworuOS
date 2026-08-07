2af5c5a3ba2911e40ddd6c86dbcaf39657a56f13
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-08 01:56:19 +0530

    sched: scheduler: add a bunch of assertion to maintain sanity as scheduler grows
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- kernel/sched/scheduler.c
+++ kernel/sched/scheduler.c
@@ -6,6 +6,7 @@
 #include "core/task.h"
 #include "debug/log.h"
 #include "debug/panic.h"
+#include "sync/run_queue.h"
 #include "sync/wait_queue.h"
 #include <stdint.h>
 
@@ -24,6 +25,12 @@ void yield(void)
  */
 void scheduler_enqueue(Task *task)
 {
+	ASSERT(task->cpu != nullptr, "task cpu is null");
+	ASSERT(task->state != TASK_RUNNING, "cannot enqueue a running task");
+	ASSERT(task->state != TASK_BLOCKED, "cannot enqueue a blocked task");
+	ASSERT(task->waiting_on == nullptr, "cannot enqueue task that is "
+					    "waiting");
+
 	Cpu *cpu = (Cpu *)task->cpu;
 	task->waiting_on = nullptr;
 	task->state = TASK_READY;
@@ -35,6 +42,11 @@ void scheduler_enqueue(Task *task)
  */
 void scheduler_dequeue(Task *task)
 {
+	ASSERT(task->cpu != nullptr, "task cpu is null");
+	ASSERT(task->state == TASK_READY, "only ready tasks can be dequeued");
+	ASSERT(task->waiting_on == nullptr, "runnable task is waiting on a "
+					    "wait queue");
+
 	Cpu *cpu = (Cpu *)task->cpu;
 	runqueue_remove(&cpu->runnable_tasks, task);
 }
@@ -49,11 +61,14 @@ void scheduler_block_current(WaitQueue *wq)
 
 	ASSERT(current != nullptr, "current is null");
 	ASSERT(current != cpu->idle, "idle task cannot block");
+	ASSERT((Cpu *)current->cpu == cpu, "current task belongs to another "
+					   "cpu");
+	ASSERT(TASK_RUNNING == current->state, "only running task can block");
+	ASSERT(current->waiting_on == nullptr, "running task is already "
+					       "waiting");
 
 	current->state = TASK_BLOCKED;
-
 	waitqueue_enqueue(wq, current);
-
 	scheduler_switch();
 
 	/*
@@ -71,6 +86,13 @@ void scheduler_wake_one(WaitQueue *wq)
 	if (nullptr == blocked) {
 		return;
 	}
+
+	ASSERT(TASK_BLOCKED == blocked->state, "wait queue contains "
+					       "non blocked task");
+	ASSERT(blocked->waiting_on == nullptr ||
+		       (WaitQueue *)blocked->waiting_on == wq,
+	       "task is waiting on a different wait queue");
+
 	scheduler_enqueue(blocked);
 }
 
@@ -84,6 +106,13 @@ void scheduler_wake_all(WaitQueue *wq)
 		if (nullptr == blocked) {
 			return;
 		}
+
+		ASSERT(blocked->state == TASK_BLOCKED, "wait queue contains "
+						       "non-blocked task");
+		ASSERT(blocked->waiting_on == nullptr ||
+			       (WaitQueue *)blocked->waiting_on == wq,
+		       "task is waiting on a different wait queue");
+
 		scheduler_enqueue(blocked);
 	}
 }
@@ -95,18 +124,33 @@ void scheduler_switch(void)
 
 	Task *prev = cpu->current;
 
+	if (nullptr != prev) {
+		ASSERT((Cpu *)prev->cpu == cpu, "current task belongs to "
+						"another cpu");
+
+		ASSERT(prev->state != TASK_BLOCKED ||
+			       prev->waiting_on != nullptr,
+		       "blocked task is not waiting on a wait queue");
+
+		ASSERT(prev->state == TASK_BLOCKED ||
+			       prev->waiting_on == nullptr,
+		       "non blocked task is waiting on a wait queue");
+
+		ASSERT(prev->state != TASK_READY || prev->waiting_on == nullptr,
+		       "ready task is waiting on a wait queue");
+	}
+
 	/*
 	 * if the current task is still runnable, put it back on the
 	 * run queue before selecting the next task
 	 */
 	if (nullptr != prev && prev != cpu->idle &&
 	    TASK_RUNNING == prev->state) {
-		ASSERT(prev->state != TASK_BLOCKED ||
-			       prev->waiting_on != nullptr,
-		       "task is not blocked but is waiting on something");
-		ASSERT(prev->state == TASK_BLOCKED ||
-			       prev->waiting_on == nullptr,
-		       "task is not blocked but is waiting on something");
+		ASSERT(prev->waiting_on == nullptr, "running task is waiting "
+						    "on a wait queue");
+		ASSERT((Cpu *)prev->cpu == cpu, "running task belongs to "
+						"another cpu");
+
 		prev->state = TASK_READY;
 		runqueue_enqueue(&cpu->runnable_tasks, prev);
 		TRACE("putting prev task (%s) back in runqueue", prev->name);
@@ -114,18 +158,19 @@ void scheduler_switch(void)
 
 	/* pick the next task */
 	Task *next = scheduler_policy_pick_next(cpu);
-
 	if (next == nullptr) {
 		next = cpu->idle;
 	}
 
+	ASSERT((Cpu *)next->cpu == cpu, "selected task belongs to another cpu");
+	ASSERT(next->state == TASK_READY || next == cpu->idle, "scheduler "
+							       "selected non "
+							       "ready task");
+	ASSERT(next->waiting_on == nullptr, "scheduler selected task waiting "
+					    "on a wait queue");
+
 	DEBUG("switching task from = %s to = %s", prev ? prev->name : "nil",
 	      next->name);
-
-	ASSERT(next->state != TASK_BLOCKED || next->waiting_on != nullptr,
-	       "task is not blocked but is waiting on something");
-	ASSERT(next->state == TASK_BLOCKED || next->waiting_on == nullptr,
-	       "task is not blocked but is waiting on something");
 
 	next->state = TASK_RUNNING;
 	cpu->current = next;
@@ -135,6 +180,8 @@ void scheduler_switch(void)
 	 * this happens when the run queue is empty and we're already in idle
 	 */
 	if (prev == next) {
+		ASSERT(next == cpu->idle || next->state == TASK_RUNNING,
+		       "current task is not running");
 		return;
 	}
 
@@ -148,8 +195,11 @@ void scheduler_switch(void)
 	}
 
 	/*
-	 *  first task ever scheduled on this cpu
+	 * first task ever scheduled on this cpu
 	 */
+	ASSERT(next != cpu->idle || scheduler_cpu_has_runnable_tasks() == false,
+	       "idle selected while runnable task exists");
+
 	ExecutionContext bootstrap;
 	context_switch(&bootstrap, &next->context);
 
@@ -166,6 +216,7 @@ Cpu *scheduler_pick_cpu(void)
 	usize best_count = INT64_MAX;
 	cpu_foreach(cpu)
 	{
+		ASSERT(cpu != nullptr, "cpu is null");
 		if (!cpu->online) {
 			continue;
 		}
