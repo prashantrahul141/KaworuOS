0944c2822b118f5cde7bf83c7761bb6d48cce270
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-12 14:20:35 +0530

    build: flake: fix build for arm linux and darwin
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- flake.nix
+++ flake.nix
@@ -8,38 +8,58 @@
   outputs =
     { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      ovmf = import ./nix/edk2-ovmf-stable-bins.nix { inherit pkgs; };
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      forEachSystem =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          glibc_multi
+      devShells = forEachSystem (
+        { pkgs }:
+        let
+          ovmf = import ./nix/edk2-ovmf-stable-bins.nix { inherit pkgs; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages =
+              with pkgs;
+              (lib.optionals stdenv.isLinux [
+                (if stdenv.hostPlatform.isx86_64 then glibc_multi else glibc)
+              ])
+              ++ [
+                llvmPackages.clang-unwrapped
+                llvmPackages.lld
+                llvm
+                xorriso
+                ovmf
+                qemu
+                gdb
+                dtc
+                gnumake
+                bear
+                python313Packages.kconfiglib
+              ];
 
-          llvmPackages.clang-unwrapped
-          llvmPackages.lld
-          llvm
-
-          xorriso
-          ovmf
-
-          qemu
-          gdb
-          dtc
-
-          gnumake
-          bear
-          python313Packages.kconfiglib
-        ];
-
-        shellHook = ''
-          export UEFI_FIRMWARE=${ovmf}/share/ovmf-code-aarch64.fd
-          export CC=clang
-          export LD=ld.lld
-          export OBJDUMP=llvm-objdump
-          export READELF=llvm-readelf
-        '';
-      };
+            shellHook = ''
+              export UEFI_FIRMWARE=${ovmf}/share/ovmf-code-aarch64.fd
+              export CC=clang
+              export LD=ld.lld
+              export OBJDUMP=llvm-objdump
+              export READELF=llvm-readelf
+            '';
+          };
+        }
+      );
     };
 }
