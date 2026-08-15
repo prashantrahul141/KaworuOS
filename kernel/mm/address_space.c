072f9f0153f52135a431592df9d9d25b0dfe8e3c
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-15 19:46:33 +0530

    mm: add address space for user processes
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

---
+++ kernel/mm/address_space.c
@@ -0,0 +1,61 @@
+#include "mm/address_space.h"
+#include "allocator/region.h"
+#include "memlayout.h"
+#include "config.h"
+#include "mm/kheap.h"
+#include "mm/paging.h"
+#include "mm/vmm.h"
+
+AddressSpace *address_space_create(void)
+{
+	AddressSpace *as = kalloc(sizeof(AddressSpace));
+	if (IS_ERR(as)) {
+		return ERR_TO_PTR(-ENOMEM);
+	}
+
+	as->table = paging_create_table();
+	if (IS_ERR(as->table)) {
+		kfree(as);
+		return ERR_TO_PTR(-ENOMEM);
+	}
+
+	as->user_region = region_create(USER_VM_RANGE_BASE, USER_VM_RANGE_SIZE,
+					CONFIG_VM_MAX_USER_ALLOCATIONS,
+					"address space");
+	if (IS_ERR(as->user_region)) {
+		paging_destroy_table(as->table);
+		kfree(as);
+		return ERR_TO_PTR(-ENOMEM);
+	}
+
+	return as;
+}
+
+void *address_space_alloc(AddressSpace *as, usize size, PagePerms perms,
+			  ExecPerms uxn)
+{
+	return vm_alloc(as->table, size, as->user_region, perms,
+			ATTR_INDEX_NORMAL, SHAREABLE_INNER_SHAREABLE,
+			NOT_EXECUTABLE, uxn);
+}
+
+errno_t address_space_map(AddressSpace *as, usize va, usize pa, usize size,
+			  PagePerms perms, ExecPerms uxn)
+{
+	return paging_map(as->table, va, pa, size, perms, ATTR_INDEX_NORMAL,
+			  SHAREABLE_INNER_SHAREABLE, NOT_EXECUTABLE, uxn);
+}
+
+void address_space_destroy(AddressSpace *as)
+{
+	for (usize i = 0; i < as->user_region->allocations_size; i++) {
+		RegionAllocation *alloc = &as->user_region->allocations[i];
+		if (nullptr != alloc) {
+			vm_free(as->table, alloc->va, as->user_region);
+		}
+	}
+
+	region_destroy(as->user_region);
+	paging_destroy_table(as->table);
+	kfree(as);
+}
---
+++ kernel/mm/address_space.h
@@ -0,0 +1,46 @@
+#ifndef _ADDRESS_SPACE_H_
+#define _ADDRESS_SPACE_H_
+
+/*
+ * Address space for user processes
+ *
+ * perms table:
+ *            |  PagePerms                    |  UserExec
+ *            |                               |
+ * code sect  | EL1_READ_ONLY_EL0_READ_WRITE |  EXECUTABLE
+ * data sect  | EL1_READ_ONLY_EL0_READ_WRITE |  NOT_EXECUTABLE
+ * stack sect | EL1_READ_ONLY_EL0_READ_WRITE |  NOT_EXECUTABLE
+ *
+ */
+
+#include "allocator/region.h"
+#include "mm/paging.h"
+
+typedef struct {
+	TableDescriptor *table;
+	AllocRegion *user_region;
+} AddressSpace;
+
+/*
+ * create new virtual address space
+ */
+AddressSpace *address_space_create(void);
+
+/*
+ * allocate and map size bytes memory
+ */
+void *address_space_alloc(AddressSpace *as, usize size, PagePerms perms,
+			  ExecPerms uxn);
+
+/*
+ * mapping for user space
+ */
+errno_t address_space_map(AddressSpace *as, usize va, usize pa, usize size,
+			  PagePerms perms, ExecPerms uxn);
+
+/*
+ * destroy and clean entire address space and the memory it allocated
+ */
+void address_space_destroy(AddressSpace *as);
+
+#endif // _ADDRESS_SPACE_H_
