731d9b4af4b774a93ab753b9a8c9065cff7bdbb1
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-15 23:52:04 +0530

    mm: vmm: fix use arg table instead of hard coding kernel_page_table
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- kernel/mm/vmm.c
+++ kernel/mm/vmm.c
@@ -226,7 +226,7 @@ void vm_free(TableDescriptor *table, void *addr, AllocRegion *region)
 	for (usize page = 0; page < allocation->page_count; page++) {
 		void *va = (u8 *)addr + page * PAGE_SIZE;
 		usize pa = paging_lookup(table, (usize)va);
-		paging_unmap(kernel_page_table, (usize)va, PAGE_SIZE);
+		paging_unmap(table, (usize)va, PAGE_SIZE);
 		pmm_free(pa);
 	}
 
