ff30a647513f79848853205baebb9fd24d35b2e3
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-21 21:20:43 +0530

    core: syscall: use syscall table instead of a huge switch
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- kernel/core/syscall.c
+++ kernel/core/syscall.c
@@ -1,46 +1,23 @@
 #include "core/syscall.h"
-#include "core/process.h"
+#include "aarch64/exception.h"
+#include "core/syscall_table.h"
 #include "error.h"
-#include "io/io.h"
 
 bool syscall_dispatch(ExceptionFrame *frame)
 {
-	i64 ret = 0;
+	i64 ret = EOK;
 	bool should_resched = false;
-	switch (frame->x8) {
-	case SYS_READ:
-	case SYS_WRITE: {
-		// x0 contains fd
-		// x1 contains buf
-		// x2 contains buf count
-		IOEvent e = { .bg = IO_DEFAULT_COLOR_BG,
-			      .fg = IO_DEFAULT_COLOR_FG,
-			      .msg = (i8 *)frame->x1,
-			      .len = frame->x2 };
-		console_write(e);
-		break;
-	}
 
-	case SYS_OPEN:
-	case SYS_CLOSE:
-	case SYS_CLONE:
-	case SYS_FORK:
-	case SYS_VFORK:
-	case SYS_EXECVE:
-	case SYS_WAIT4:
-	case SYS_KILL:
-	case SYS_UNAME:
-		break;
-	case SYS_YIELD:
-		should_resched = true;
-		break;
-	case SYS_EXIT:
-		sys_exit((i32)frame->x0);
-		break;
-	default:
+	syscall_function_type fn = syscall_table_retrieve(frame->x8);
+	if (nullptr == fn) {
 		ret = -ENOSYS;
 		WARN("unknown syscall: %d", frame->x8);
+		return should_resched;
 	}
+
+	SyscallReturn syscall_ret = fn(frame);
+	ret = syscall_ret.ret;
+	should_resched = syscall_ret.should_resched;
 
 	frame->x0 = (u64)ret;
 	return should_resched;
