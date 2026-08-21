45216eb2805a2cfb2b940a952b4c22f533406661
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-22 00:40:10 +0530

    core: syscall: move SYS_* enum to syscall table
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- kernel/core/syscall.h
+++ kernel/core/syscall.h
@@ -3,22 +3,6 @@
 
 #include "aarch64/exception.h"
 
-enum {
-	SYS_READ = 0,
-	SYS_WRITE = 1,
-	SYS_OPEN = 2,
-	SYS_CLOSE = 3,
-	SYS_YIELD = 24,
-	SYS_CLONE = 56,
-	SYS_FORK = 57,
-	SYS_VFORK = 58,
-	SYS_EXECVE = 59,
-	SYS_EXIT = 60,
-	SYS_WAIT4 = 61,
-	SYS_KILL = 62,
-	SYS_UNAME = 63,
-};
-
 /* helper to check if exception is a syscall */
 static inline bool is_syscall(ExceptionFrame *frame)
 {
--- kernel/core/syscall_table.h
+++ kernel/core/syscall_table.h
@@ -4,6 +4,24 @@
 #include "aarch64/exception.h"
 #include "types.h"
 
+enum {
+	SYS_READ = 0,
+	SYS_WRITE = 1,
+	SYS_OPEN = 2,
+	SYS_CLOSE = 3,
+	SYS_YIELD = 24,
+	SYS_GETPID = 39,
+	SYS_CLONE = 56,
+	SYS_FORK = 57,
+	SYS_VFORK = 58,
+	SYS_EXECVE = 59,
+	SYS_EXIT = 60,
+	SYS_WAIT4 = 61,
+	SYS_KILL = 62,
+	SYS_UNAME = 63,
+	SYS_GETPPID = 110,
+};
+
 typedef struct {
 	i64 ret;
 	bool should_resched;
