4927a77e5809f9ac06c16309133a3008ed029505
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-22 15:41:45 +0530

    core: syscall: clean up dispatch function
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- kernel/core/syscall.c
+++ kernel/core/syscall.c
@@ -5,20 +5,15 @@
 
 bool syscall_dispatch(ExceptionFrame *frame)
 {
-	i64 ret = EOK;
-	bool should_resched = false;
-
+	DEBUG("dispatching: %d", frame->x8);
 	syscall_function_type fn = syscall_table_retrieve(frame->x8);
 	if (nullptr == fn) {
-		ret = -ENOSYS;
+		frame->x0 = (u64)-ENOSYS;
 		WARN("unknown syscall: %d", frame->x8);
-		return should_resched;
+		return false;
 	}
 
 	SyscallReturn syscall_ret = fn(frame);
-	ret = syscall_ret.ret;
-	should_resched = syscall_ret.should_resched;
-
-	frame->x0 = (u64)ret;
-	return should_resched;
+	frame->x0 = (u64)syscall_ret.ret;
+	return syscall_ret.should_resched;
 }
