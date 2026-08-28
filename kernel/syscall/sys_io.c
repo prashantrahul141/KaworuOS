d8787d3be684c39faf5cdcdea33eb3bcc8a253f6
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-28 22:22:23 +0530

    syscall: sys_io: write: hook fd_table to write syscall
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- kernel/syscall/sys_io.c
+++ kernel/syscall/sys_io.c
@@ -1,5 +1,6 @@
 #include "syscall/sys_io.h"
-#include "io/io.h"
+#include "core/process.h"
+#include "debug/assert.h"
 #include "mm/kheap.h"
 #include "mm/uaccess.h"
 #include "syscall/syscall_table.h"
@@ -7,9 +8,11 @@
 SYSCALL_DEFINE3(write, frame, i64, fd, i8 *, buf, usize, count)
 {
 	DEBUG("SYS_WRITE: fd = %d buf = %p count = %d", fd, buf, count);
-
 	UNUSED_ARG(frame);
-	UNUSED_ARG(fd);
+
+	if (0 == count) {
+		return (SyscallReturn){ .should_resched = false, .ret = EOK };
+	}
 
 	i8 *kernel_buf = kalloc(count);
 	if (IS_ERR(kernel_buf)) {
@@ -25,12 +28,21 @@ SYSCALL_DEFINE3(write, frame, i64, fd, i8 *, buf, usize, count)
 		return (SyscallReturn){ .should_resched = false, .ret = ret };
 	}
 
-	IOEvent ev = io_event_default(buf, count);
-	ret = console_write(ev);
-	if (EOK == ret) {
-		ret = (i64)count;
+	Process *proc = process_get_current();
+	ASSERT(!IS_ERR(proc), "proc is null");
+
+	File *file = fdtable_get_file(proc->files, (usize)fd);
+	if (IS_ERR(file)) {
+		kfree(kernel_buf);
+		WARN("no fd = %d found", fd);
+		return (SyscallReturn){ .should_resched = false,
+					.ret = PTR_TO_ERR(file) };
 	}
 
+	i64 written = file_write(file, kernel_buf, count);
+
+	file_put(file);
 	kfree(kernel_buf);
-	return (SyscallReturn){ .should_resched = false, .ret = ret };
+
+	return (SyscallReturn){ .should_resched = false, .ret = written };
 }
