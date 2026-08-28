0b8c9a40cbfc0d3d322fe827be2ef9359e16b4e4
Prashant Rahul <prashantrahul141@protonmail.com>
2026-08-28 22:27:44 +0530

    user: user_init: more testing code
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- user/user_init.c
+++ user/user_init.c
@@ -22,6 +22,13 @@ static UNUSED void sys_exit(i64 status)
 	asm volatile("svc #0" : : "r"(x0), "r"(x8) : "memory");
 }
 
+static UNUSED void sys_wait4(i64 *status)
+{
+	register i64 *x0 asm("x0") = status;
+	register u64 x8 asm("x8") = SYS_WAIT4;
+	asm volatile("svc #0" : : "r"(x0), "r"(x8) : "memory");
+}
+
 static UNUSED i64 sys_getpid(void)
 {
 	register i64 x0 asm("x0") = 0;
@@ -46,26 +53,60 @@ static UNUSED i64 sys_fork(void)
 	return x0;
 }
 
+static UNUSED void sys_call(usize id)
+{
+	register u64 x8 asm("x8") = id;
+	asm volatile("svc #0" : : "r"(x8) : "memory");
+}
+
 void _start(void)
 {
-	i8 msg[13];
-	msg[0] = 0x68;
-	msg[1] = 0x65;
-	msg[2] = 0x6C;
-	msg[3] = 0x6C;
-	msg[4] = 0x6F;
-	msg[5] = 0x20;
-	msg[6] = 0x79;
-	msg[7] = 0x61;
-	msg[8] = 0x61;
-	msg[9] = 0x72;
-	msg[10] = 0x61;
-	msg[11] = 0x6E;
-	msg[12] = 0x0A;
+	i8 msg1[13];
+	msg1[0] = 0x68;
+	msg1[1] = 0x65;
+	msg1[2] = 0x6C;
+	msg1[3] = 0x6C;
+	msg1[4] = 0x6F;
+	msg1[5] = 0x20;
+	msg1[6] = 0x79;
+	msg1[7] = 0x61;
+	msg1[8] = 0x61;
+	msg1[9] = 0x72;
+	msg1[10] = 0x61;
+	msg1[11] = 0x6E;
+	msg1[12] = 0x0A;
 
-	i64 pid = sys_fork();
+	i8 msg2[14];
+	msg2[0] = 0x62;
+	msg2[1] = 0x72;
+	msg2[2] = 0x76;
+	msg2[3] = 0x74;
+	msg2[4] = 0x61;
+	msg2[5] = 0x6c;
+	msg2[6] = 0x20;
+	msg2[7] = 0x79;
+	msg2[8] = 0x61;
+	msg2[9] = 0x61;
+	msg2[10] = 0x72;
+	msg2[11] = 0x61;
+	msg2[12] = 0x6E;
+	msg2[13] = 0x0A;
+
+	i64 ret = sys_fork();
+	if (ret == 0) {
+		// child
+		for (usize i = 0; i < 10; i++) {
+			// 2 == stderr
+			sys_write(2, msg2, sizeof(msg2));
+		}
+	} else {
+		// parent
+		for (usize i = 0; i < 10; i++) {
+			// 1 == stdout
+			sys_write(1, msg1, sizeof(msg1));
+		}
+	}
+
 	for (;;) {
-		i8 a = (i8)pid + 65;
-		sys_write(1, &a, 1);
 	}
 }
