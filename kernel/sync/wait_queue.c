afa2b6a1089ff5b5b47928eacdb94f8ef403e21e
Prashant Rahul <prashantrahul141@protonmail.com>
2026-09-04 05:19:51 +0530

    sync: wait_queue: add unlocked variants of waitqueue functions
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- kernel/sync/wait_queue.c
+++ kernel/sync/wait_queue.c
@@ -13,16 +13,52 @@ void waitqueue_init(WaitQueue *wq, const i8 *name)
 
 void waitqueue_enqueue(WaitQueue *wq, Task *task)
 {
-	ASSERT(task->state != TASK_READY, "only NOT READY tasks may enter "
-					  "waitqueue");
 	spinlock_acquire_scoped(&wq->lock);
-	intrusivelist_insert_tail(&wq->waiters, &task->wait_node);
-	task->waiting_on = (void *)wq;
+	waitqueue_enqueue_unlocked(wq, task);
 }
 
 Task *waitqueue_dequeue(WaitQueue *wq)
 {
 	spinlock_acquire_scoped(&wq->lock);
+	return waitqueue_dequeue_unlocked(wq);
+}
+
+void waitqueue_remove(WaitQueue *wq, Task *task)
+{
+	spinlock_acquire_scoped(&wq->lock);
+	waitqueue_remove_unlocked(wq, task);
+}
+
+bool waitqueue_is_empty(WaitQueue *wq)
+{
+	spinlock_acquire_scoped(&wq->lock);
+	return waitqueue_is_empty_unlocked(wq);
+}
+
+usize waitqueue_count(WaitQueue *wq)
+{
+	spinlock_acquire_scoped(&wq->lock);
+	return waitqueue_count_unlocked(wq);
+}
+
+Task *waitqueue_peek(WaitQueue *wq)
+{
+	spinlock_acquire_scoped(&wq->lock);
+	return waitqueue_peek_unlocked(wq);
+}
+
+void waitqueue_enqueue_unlocked(WaitQueue *wq, Task *task)
+{
+	ASSERT(task->state != TASK_READY, "only NOT READY tasks may enter "
+					  "waitqueue");
+	spinlock_assert_locked(&wq->lock);
+	intrusivelist_insert_tail(&wq->waiters, &task->wait_node);
+	task->waiting_on = (void *)wq;
+}
+
+Task *waitqueue_dequeue_unlocked(WaitQueue *wq)
+{
+	spinlock_assert_locked(&wq->lock);
 	IntrusiveNode *wait_node = intrusivelist_remove_head(&wq->waiters);
 	if (nullptr == wait_node) {
 		return nullptr;
@@ -32,27 +68,27 @@ Task *waitqueue_dequeue(WaitQueue *wq)
 	return waiting_task;
 }
 
-void waitqueue_remove(WaitQueue *wq, Task *task)
+void waitqueue_remove_unlocked(WaitQueue *wq, Task *task)
 {
-	spinlock_acquire_scoped(&wq->lock);
+	spinlock_assert_locked(&wq->lock);
 	intrusivelist_remove(&wq->waiters, &task->wait_node);
 }
 
-bool waitqueue_is_empty(WaitQueue *wq)
+bool waitqueue_is_empty_unlocked(WaitQueue *wq)
 {
-	spinlock_acquire_scoped(&wq->lock);
+	spinlock_assert_locked(&wq->lock);
 	return intrusivelist_is_empty(&wq->waiters);
 }
 
-usize waitqueue_count(WaitQueue *wq)
+usize waitqueue_count_unlocked(WaitQueue *wq)
 {
-	spinlock_acquire_scoped(&wq->lock);
+	spinlock_assert_locked(&wq->lock);
 	return intrusivelist_count(&wq->waiters);
 }
 
-Task *waitqueue_peek(WaitQueue *wq)
+Task *waitqueue_peek_unlocked(WaitQueue *wq)
 {
-	spinlock_acquire_scoped(&wq->lock);
+	spinlock_assert_locked(&wq->lock);
 	IntrusiveNode *wait_node = intrusivelist_peek_head(&wq->waiters);
 	if (nullptr == wait_node) {
 		return nullptr;
--- kernel/sync/wait_queue.h
+++ kernel/sync/wait_queue.h
@@ -11,11 +11,23 @@ typedef struct {
 } WaitQueue;
 
 void waitqueue_init(WaitQueue *wq, const i8 *name);
+
 void waitqueue_enqueue(WaitQueue *wq, Task *task);
 Task *waitqueue_dequeue(WaitQueue *wq);
 void waitqueue_remove(WaitQueue *q, Task *task);
 bool waitqueue_is_empty(WaitQueue *q);
 usize waitqueue_count(WaitQueue *rq);
 Task *waitqueue_peek(WaitQueue *q);
+
+/*
+ * the callee should gurantee that it holds the internal lock before doing these
+ * operations
+ */
+void waitqueue_enqueue_unlocked(WaitQueue *wq, Task *task);
+Task *waitqueue_dequeue_unlocked(WaitQueue *wq);
+void waitqueue_remove_unlocked(WaitQueue *wq, Task *task);
+bool waitqueue_is_empty_unlocked(WaitQueue *wq);
+usize waitqueue_count_unlocked(WaitQueue *wq);
+Task *waitqueue_peek_unlocked(WaitQueue *wq);
 
 #endif // _WAIT_QUEUE_H_
